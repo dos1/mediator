9fd9a3ccb33ca91974fc6e471de2089504f8c399
Sebastian Krzyszkowiak <dos@dosowisko.net>
2015-10-17 04:52:18 +0200

    highscore handling

--- src/gamestates/theend.c
+++ src/gamestates/theend.c
@@ -26,6 +26,7 @@
 
 #include "../utils.h"
 #include "../timeline.h"
+#include "../config.h"
 #include "theend.h"
 
 int Gamestate_ProgressCount = 1;
@@ -37,14 +38,27 @@ void Gamestate_Logic(struct Game *game, struct dosowiskoResources* data) {
 void Gamestate_Draw(struct Game *game, struct dosowiskoResources* data) {
     al_draw_bitmap(data->bitmap, 0, 0, 0);
     char text[255];
-    snprintf(text, 255, "The End");
-    DrawTextWithShadow(game->_priv.font, al_map_rgb(255,255,255), 320/2, 180/2, ALLEGRO_ALIGN_CENTER, text);
+    snprintf(text, 255, "%d", game->mediator.score);
+    DrawTextWithShadow(game->_priv.font, al_map_rgb(255,255,255), 320/2, 20, ALLEGRO_ALIGN_CENTER, "Score:");
+    DrawTextWithShadow(data->font, al_map_rgb(255,255,255), 320/2, 30, ALLEGRO_ALIGN_CENTER, text);
+    snprintf(text, 255, "High score: %d", data->score);
+    DrawTextWithShadow(game->_priv.font, al_map_rgb(255,255,255), 320/2, 140, ALLEGRO_ALIGN_CENTER, text);
+    DrawTextWithShadow(game->_priv.font, al_map_rgb(255,255,255), 320/2, 162, ALLEGRO_ALIGN_CENTER, "Press RETURN");
 }
 
 void Gamestate_Start(struct Game *game, struct dosowiskoResources* data) {
     al_set_sample_instance_gain(game->muzyczka.instance.drums, 0.0);
     al_set_sample_instance_gain(game->muzyczka.instance.fg, 0.0);
     al_set_sample_instance_gain(game->muzyczka.instance.bg, 1.5);
+
+    char *end = "";
+    data->score = strtol(GetConfigOptionDefault(game, "Mediator", "score", "0"), &end, 10);
+
+    if (game->mediator.score > data->score) {
+        char text[255];
+        snprintf(text, 255, "%d", game->mediator.score);
+        SetConfigOption(game, "Mediator", "score", text);
+    }
 }
 
 void Gamestate_ProcessEvent(struct Game *game, struct dosowiskoResources* data, ALLEGRO_EVENT *ev) {
@@ -52,13 +66,16 @@ void Gamestate_ProcessEvent(struct Game *game, struct dosowiskoResources* data, 
 	if ((ev->type==ALLEGRO_EVENT_KEY_DOWN) && (ev->keyboard.keycode == ALLEGRO_KEY_ESCAPE)) {
         SwitchGamestate(game, "theend", "menu");
 	}
+    if ((ev->type==ALLEGRO_EVENT_KEY_DOWN) && (ev->keyboard.keycode == ALLEGRO_KEY_ENTER)) {
+        SwitchGamestate(game, "theend", "menu");
+    }
 }
 
 void* Gamestate_Load(struct Game *game, void (*progress)(struct Game*)) {
 	struct dosowiskoResources *data = malloc(sizeof(struct dosowiskoResources));
     data->bitmap = al_load_bitmap( GetDataFilePath(game, "bg.png"));
 
-	data->font = al_load_ttf_font(GetDataFilePath(game, "fonts/DejaVuSansMono.ttf"),game->viewport.height*0.1666,0 );
+    data->font = al_load_ttf_font(GetDataFilePath(game, "fonts/MonkeyIsland.ttf"),100,0 );
 	(*progress)(game);
 
 	return data;
